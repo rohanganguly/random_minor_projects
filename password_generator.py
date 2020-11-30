0251c4b74e4e160bc1b9e7963f3d270788bebf13
rohanganguly <rohanganguly66@gmail.com>
2020-12-01 01:20:12 +0530

    commit on 'main'

--- password_generator.py
+++ password_generator.py
@@ -10,13 +10,19 @@ import secrets
 
 characters= string.ascii_uppercase + string.ascii_lowercase + string.digits 
 
+#to check the conditions, we start an while loop set to the default value of true
+
 while True: 
+    #we first create the password using the secret library and a for loop
     password= "".join(secrets.choice(characters) for i in range(16))
-        
+    
+    #checks whether the conditions are fullfilled    
     if (any(c.islower() for c in password) 
         and any(c.isupper()for c in password)
         and sum(c.isdigit()for c in password)>=4):
-        break
+        break #breaks the loop when conditions are met
 
 print (password)
+
+#end of program
 
